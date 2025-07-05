2ee0fa363486e813f9ccb1a811f889da8707bea9
xavier.fremault <xavier.fremault@gmail.com>
2025-07-05 06:22:44 +0200

    Ajout des boutons filtres, ils ne st pas encore fonctionnels

--- script.js
+++ script.js
@@ -22,8 +22,39 @@ async function gallery() {
     img.alt = works[i].title;
     figcaption.innerText = `${works[i].title}`;
 
-    console.log(gallery);
+    // console.log(gallery);
   }
 }
 
 gallery();
+
+// Fonction pour le Filtre
+async function categories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+  console.log(categories);
+
+  // Ciblage de ma balise <h2> dans <section id="portfolio">
+  const portfolio = document.getElementById("portfolio");
+  console.log(portfolio);
+  //   Creation de ma <div> qui contiendra mes boutons filtres et rattachement à portfolio
+  const filter = document.createElement("div");
+  const gallery = document.querySelector(".gallery");
+
+  filter.classList.add("filter");
+  portfolio.insertBefore(filter, gallery);
+
+  //   Creation de mon bouton "Tous" rattaché à ma <div class=filter>
+  const filterTous = document.createElement("button");
+  filterTous.innerText = "Tous";
+  filter.appendChild(filterTous);
+
+  //   Creation d'une boucle FOR pour chq élt de mon tableau categories
+  for (let i = 0; i < categories.length; i++) {
+    const btn = document.createElement("button");
+    filter.appendChild(btn);
+    btn.innerText = categories[i].name;
+  }
+}
+
+categories();
